faf6d8bb4f063ef77192fb70eb4d016d143a5953
botones de redes <gonzarobledo96@gmail,com>
2024-01-11 15:07:54 -0300

    Primer commit

---
+++ prueba.js
@@ -0,0 +1,15 @@
+const likeButtons = document.querySelectorAll(".card__btn");
+
+likeButtons.forEach((likeButton) => {
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__btn--like");
+  });
+});
+
+
+
+
+
+
+
+
